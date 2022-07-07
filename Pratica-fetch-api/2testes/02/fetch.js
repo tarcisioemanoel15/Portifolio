98f533761686dc4c98137befede13ea094c17fd3
Tarcisio <caninosbrancos10031@gmail.com>
2022-07-07 15:40:51 -0300

    pratica fetch github

--- Pratica-fetch-api/2testes/02/fetch.js
+++ Pratica-fetch-api/2testes/02/fetch.js
@@ -1,1 +1,66 @@
-fetch('https://wwhereby.com/charming-enthusiastic-will')
+function inicia() {
+  const nomeinput = document.querySelector('.busca').value;
+  if (!nomeinput) {
+
+    fetch(`https://api.github.com/users/tarcisioemanoel15`)
+      .then(response => response.json())
+      .then(json => extraindo(json))
+      .catch(e => console.log('Deu ERRO', e))
+
+    document.addEventListener('click', e => {
+      const el = e.target;
+      if (el.classList.contains('repo')) {
+        fetch(`https://api.github.com/users/tarcisioemanoel15/repos`)
+          .then(response => response.json())
+          .then(json => extraindorepo(json))
+          .catch(e => console.log('Deu ERRO', e))
+      }
+    })
+
+  } else {
+    fetch(`https://api.github.com/users/${nomeinput}`)
+      .then(response => response.json())
+      .then(json => extraindo(json))
+      .catch(e => console.log('Deu ERRO', e))
+    document.addEventListener('click', e => {
+      const el = e.target;
+      if (el.classList.contains('repo')) {
+        fetch(`https://api.github.com/users/${nomeinput}/repos`)
+          .then(response => response.json())
+          .then(json => extraindorepo(json))
+          .catch(e => console.log('Deu ERRO', e))
+      }
+    })
+  }
+}
+
+function extraindo(json) {
+  const section = document.querySelector('section');
+  section.innerHTML = `
+  <h1>${json.name}</h1>
+  <img src="${json.avatar_url}" alt="">
+  `
+}
+
+function extraindorepo(json) {
+  const ul = document.querySelector('ul');
+  const tesst = json.map(element => {
+    const li = document.createElement('li');
+    li.classList.add('item')
+    li.innerHTML = `${element.name}`;
+    ul.appendChild(li)
+  })
+  document.addEventListener('click', e => {
+    const el = e.target;
+    if (el.classList.contains('inicia')) {
+      for (let campo of document.querySelectorAll('li')) {
+        campo.remove();
+      }
+    }
+    if (el.classList.contains('repo')) {
+      for (let camporepo of document.querySelectorAll('.item')) {
+        camporepo.remove();
+      }
+    }
+  })
+}
