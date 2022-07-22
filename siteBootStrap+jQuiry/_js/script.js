fb0e6c2575ad6aef7843c841ad7995bdca09f5e7
Tarcisio <caninosbrancos10031@gmail.com>
2022-07-22 16:41:42 -0300

    a

--- siteBootStrap+jQuiry/_js/script.js
+++ siteBootStrap+jQuiry/_js/script.js
@@ -1,3 +1,43 @@
+function addFotos() {
+
+  const fotos = document.querySelector('#fotos');
+  const objIMG = [
+
+    { 'id': '1', 'url': '../_img/dragonballz/1.jpeg' }
+
+  ];
+
+  const player = (foto, local) => {
+    fotos.forEach(element => {
+      fotos.innerHTML += `
+      <div class="item">
+      <img src="${element.url}" alt="">
+      </div>
+      
+      ${element}
+      `;
+
+    });
+  }
+
+
+  player(objIMG, fotos)
+
+
+
+}
+addFotos();
+
+
+
+
+
+
+
+
+
+
+
 $(function () {
   $('.carousel').carousel({
     interval: 2000,
