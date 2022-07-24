7cb7a3718386b87228bbe500711fbe4c249361de
Tarcisio <caninosbrancos10031@gmail.com>
2022-07-24 17:51:28 -0300

    teste imagem no github

--- siteBootStrap+jQuiry/js/script.js
+++ siteBootStrap+jQuiry/js/script.js
@@ -1,3 +1,31 @@
+function buscarIMG() {
+  const paisagen = document.querySelector('.paisagen')
+  //const retrato = document.querySelector('.retrato')
+  fetch('./imagen.json')
+    .then(response => response.json())
+    .then(json => inseririmg1(json, paisagen))
+    .catch(e => console.log(e, 'DEU ERRO'));
+}
+
+function inseririmg1(json, campo) {
+  json.forEach(element => {
+    campo.innerHTML += `
+    <div class="images1"><img src="${element.url}" alt="image"></div>
+    `;
+  });
+  larguraMaior();
+}
+
+function larguraMaior() {
+  for (let imgs of document.querySelectorAll('.images1 img')) {
+    if ($(imgs).width() > $(imgs).height()) {
+      imgs.classList.add('largura');
+    } else {
+      imgs.classList.add('altura');
+    }
+  }
+}
+
 $(function () {
   $('.carousel').carousel({
     interval: 2000,
@@ -5,3 +33,5 @@ $(function () {
     wrap: true
   })
 })
+
+buscarIMG();
